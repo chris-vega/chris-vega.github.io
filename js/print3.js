dc21bc1b4115d75e0b91a560c0e6b523f83f9266
Chris Vega <84395368+chris-vega@users.noreply.github.com>
2023-12-18 09:36:16 -0500

    cleanup

--- js/print3.js
+++
@@ -1,8 +0,0 @@
-// Select the button element
-const pdfButton = document.getElementById("pdf-button3");
-
-// Add a click event listener to the button
-pdfButton.addEventListener("click", function () {
-  // Call the print function to open the print dialog
-  window.print();
-});
